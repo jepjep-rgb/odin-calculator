c7af54b88e943edae1f56e71957801e714e7b3bd
jepjep-rgb <jefroyalde@gmail.com>
2022-08-02 10:12:02 +0800

    Add operate function to call appropriate operation
    The operateNumber function splits the string input into two arrays:
    an digit array, and the operator array. The function checks the
    operator and calls the relevant operation function. The function
    call uses the digit numbers in the number array as arguments.

--- calculator.js
+++ calculator.js
@@ -13,3 +13,22 @@ function multiplyNumbers(num1, num2){
 function divideNumbers(num1, num2){
     return num1 / num2;
 }
+
+function operateNumbers(input){
+    let inputArray = input.split(/[\+\-\*\/]/);
+    let operateArray = input.split(/\d/);
+    for (let i = 0; i < operateArray.length; i++) {
+        if (operateArray[i] === ""){
+            operateArray.splice(i, 1);
+            i--;
+        }
+    }
+    console.log(inputArray);
+    console.log(operateArray);
+    if (operateArray[0] === '+') console.log(addNumbers(Number(inputArray[0]), Number(inputArray[1])));
+    else if (operateArray[0] === '-') console.log(subtractNumbers(Number(inputArray[0]), Number(inputArray[1])));
+    else if (operateArray[0] === '*') console.log(multiplyNumbers(Number(inputArray[0]), Number(inputArray[1])));
+    else if (operateArray[0] === '/') console.log(divideNumbers(Number(inputArray[0]), Number(inputArray[1])));
+
+
+}
